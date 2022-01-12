257f0ad81cc8d38ddc193c0a71f9f9ed9e238c25
RomanBobkovTKI <RomanBobkovTKI>
2022-01-13 01:18:05 +0300

    Vs files add

--- SUBD/SUBD.cpp
+++ SUBD/SUBD.cpp
@@ -1,12 +1,23 @@
-#include <Header.h>
+include <cassert>
+#include "Header.h"
 
 using namespace std;
+
+Stack::Stack(const int maxSize) :
+    size(maxSize)
+{
+    stackPtr = new Stack[size];
+    top = 0;
+}
+
+
+Stack::~Stack()
+{
+    delete[] stackPtr;
+}
+
+
 
 
 
 int main()
-{
-       
-}
-
-
